2f1512de56788036f1b3d646becbf6dd9dcdb173
Eliasw <32161114+MyxomatosisAstro@users.noreply.github.com>
2020-02-18 00:06:42 +0100

    Add files via upload

---
+++ ApiService.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Weather_Report
+{
+	// This is our generic API service, returning data from the SMHI API with a specified endpoint
+    static class ApiService
+    {
+		public static async Task<string> GetDataAsync(string endPoint)
+		{
+			
+			try
+			{
+
+				using (var httpClient = new HttpClient { Timeout = TimeSpan.FromSeconds(10) })
+				using (HttpResponseMessage response =
+				    await httpClient.GetAsync(new Uri("http://opendata-download-metobs.smhi.se/api/" + endPoint)))
+				{					
+					return await response.Content.ReadAsStringAsync();
+				}
+
+
+			}
+
+			// If the request fails prints the exception
+			catch (HttpRequestException e)
+
+
+			{
+				Console.WriteLine(e);
+				return string.Empty;
+			}
+
+
+		}
+	}
+}
---
+++ Model.cs
@@ -0,0 +1,88 @@
+﻿
+// A class containing all the different levels (or classes) in the retrieved data
+
+
+namespace Weather_Report
+{
+
+    public class Stations
+    {
+        public Station[] station { get; set; }
+        public long updated { get; set; }
+        public Parameter parameter { get; set; }
+        public Link[] link { get; set; }
+    }
+
+    public class Parameter
+    {
+        public string key { get; set; }
+        public string name { get; set; }
+        public string summary { get; set; }
+        public string unit { get; set; }
+    }
+
+    public class Station
+    {
+        public string key { get; set; }
+        public string name { get; set; }
+        public string owner { get; set; }
+        public long from { get; set; }
+        public long to { get; set; }
+        public float height { get; set; }
+        public float latitude { get; set; }
+        public float longitude { get; set; }
+        public Value[] value { get; set; }
+    }
+
+    public class Value
+    {
+        public long date { get; set; }
+        public string value { get; set; }
+        public string quality { get; set; }
+    }
+
+    public class Link
+    {
+        public string rel { get; set; }
+        public string type { get; set; }
+        public string href { get; set; }
+    }
+
+    public class StationCurrentTemp
+    {
+        public string key { get; set; }
+        public long updated { get; set; }
+        public string title { get; set; }
+        public string owner { get; set; }
+        public bool active { get; set; }
+        public string summary { get; set; }
+        public long from { get; set; }
+        public long to { get; set; }
+        public Period[] period { get; set; }
+    }
+
+    public class Period
+    {
+        public string key { get; set; }
+        public long updated { get; set; }
+        public string title { get; set; }
+        public string summary { get; set; }
+        public Link[] link { get; set; }
+    }
+
+    public class StationTemp
+    {
+        public TempValue[] value { get; set; }
+        public StationTempName station { get; set; }
+    }
+
+    public class StationTempName
+    {
+        public string name { get; set; }
+    }
+
+    public class TempValue
+    {
+        public string value { get; set; }
+    }
+}
